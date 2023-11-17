369052587537cacb3249a156c5786486e4826ec0
ericthelemur <owenconnors1@gmail.com>
2023-11-17 02:19:05 +0000

    Approve if reading

--- src/extension/messages.ts
+++ src/extension/messages.ts
@@ -1,7 +1,7 @@
 import type NodeCG from '@nodecg/types';
 import type { Donation } from '../types/schemas';
 import { getNodeCG } from './utils';
-import { ModStatus } from './utils/mod';
+import { ModStatus, UNDECIDED } from './utils/mod';
 import * as rep from './utils/replicants';
 
 const nodecg = getNodeCG();
@@ -25,7 +25,7 @@ nodecg.listenFor("approve-all-donations", (value, ack) => {
 });
 
 
-function searchAndSet<T>(id: string, prop: string, value: T, ack: NodeCG.Acknowledgement | undefined) {
+function searchAndSet<T>(id: string, prop: string, value: T, ack: NodeCG.Acknowledgement | undefined): Donation | undefined {
     nodecg.log.info("Mark", prop, id, value);
     var elementIndex = rep.donations.value.findIndex((d: Donation) => d.id === id);
     if (elementIndex !== -1) {
@@ -35,22 +35,25 @@ function searchAndSet<T>(id: string, prop: string, value: T, ack: NodeCG.Acknowl
         if (ack && !ack.handled) {
             ack(null, null);
         }
+        return rep.donations.value[elementIndex];
     } else {
         if (ack && !ack.handled) {
             nodecg.log.error('Donation not found to mark as read | id:', id);
             ack(new Error("Donation not found to mark as read"), null);
         }
+        return undefined;
     }
 }
 
 nodecg.listenFor("set-donation-read", ([dono, readVal], ack) => {
-    searchAndSet<boolean>(dono.id, "read", readVal, ack);
+    const d = searchAndSet(dono.id, "read", readVal, ack);
+    if (d && readVal && d.modStatus === UNDECIDED) d.modStatus = true;
 });
 
 nodecg.listenFor("set-donation-shown", ([dono, shownVal], ack) => {
-    searchAndSet<boolean>(dono.id, "shown", shownVal, ack);
+    searchAndSet(dono.id, "shown", shownVal, ack);
 });
 
 nodecg.listenFor("set-donation-modstatus", ([dono, statusVal], ack) => {
-    searchAndSet<ModStatus>(dono.id, "modStatus", statusVal, ack);
+    searchAndSet(dono.id, "modStatus", statusVal, ack);
 });
